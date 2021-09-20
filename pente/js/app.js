0929fd27db370ca6e82f04c481301005ec2068aa
AdrienDurup <adrien.durup@gmail.com>
2021-09-20 14:57:41 +0200

    gestion des bords de board.

--- pente/js/app.js
+++ pente/js/app.js
@@ -31,11 +31,11 @@ const app = {
             for (let i = 0; i < axes.length; i++) {
                 /* vérifie si on a aligné 5 pierres */
                 let score = this.checkAxis(axes[i]);
-                console.log(`score sur l’axe ${axes[i]} : ${this.score}`);
+                console.log(`score sur l’axe ${axes[i]} : ${score}`);
                 /* Verifie si on vient de capturer des paires */
                 this.checkPair(axes[i]);
                 this.checkPair([axes[i][0] * -1, axes[i][1] * -1]);
-                if (score > 5 || this.pairs === 5) {
+                if (score >= 5 || this.pairs === 5) {
                     app.gameOver();
                     break;
                 };
@@ -54,7 +54,9 @@ const app = {
         checkOneDirection(axis) {
             let score = 0;
             for (let i = 1; i < 5; i++) {
-                if (app.Cell.dictionary[`cell_${this.move[0] + axis[0] * i}_${this.move[1] + axis[1] * i}`].value === this.id) {
+                const id=`cell_${this.move[0] + axis[0] * i}_${this.move[1] + axis[1] * i}`;
+                if (app.Cell.dictionary[id]!==undefined//Si on est hors champ on doit breaker
+                    &&app.Cell.dictionary[id].value === this.id) {
                     score++;
                 } else {
                     break;
@@ -64,16 +66,25 @@ const app = {
         }
         checkPair(axis) {
             let pairCheck = [];
+            let isPairCheckValid=true;
             for (let i = 1; i < 4; i++) {
                 const id=`cell_${this.move[0] + axis[0] * i}_${this.move[1] + axis[1] * i}`;
                 let aheadCell = app.Cell.dictionary[id];
-                pairCheck.push({id:id,value:aheadCell.value});
+                if(aheadCell!==undefined){
+                    pairCheck.push({id:id,value:aheadCell.value});
+                }else{
+                    isPairCheckValid=false;
+                };
+
             }
-            if (pairCheck[0].value!==this.id
+            if (isPairCheckValid===true
+                &&pairCheck[0].value!==this.id
                 &&pairCheck[0].value === pairCheck[1].value 
                 && pairCheck[0].value !== "" 
-                && pairCheck[2].value == this.id) {
+                && pairCheck[2].value === this.id
+                ) {
                 this.pairs++;
+                console.log(this.pairs);
                 app.deleteStone(pairCheck[0].id);
                 app.deleteStone(pairCheck[1].id);
             };
